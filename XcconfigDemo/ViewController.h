f984de1efa0ff39648bc2eb0b52592b661d0b680
heweiqi123 <123610105@qq.com>
2020-01-12 22:31:46 +0800

    Initial Commit

---
+++ XcconfigDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcconfigDemo
+//
+//  Created by 贺文杰 on 2020/1/12.
+//  Copyright © 2020 贺文杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
